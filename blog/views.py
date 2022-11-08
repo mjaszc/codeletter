0b2573f2f406185762781c255611c802d858b015
mjasc <111734497+mjasc@users.noreply.github.com>
2022-11-08 20:14:29 +0100

    post information correctly displays on homepage

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def homepage(request):
-    return render(request, "blog/homepage.html")
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "blog/homepage.html", context)
