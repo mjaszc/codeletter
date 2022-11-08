5546cc10db5a015625293cfa832fd402a0cd5255
jashchanin <111734497+jashchanin@users.noreply.github.com>
2022-11-08 20:18:46 +0100

    Set homepage view (#2)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def homepage(request):
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "blog/homepage.html", context)
