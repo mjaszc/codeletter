6924c739cacc6fb532380bb4c139a93283a987ab
jashchanin <111734497+jashchanin@users.noreply.github.com>
2022-11-09 17:37:20 +0100

    Created post details view (#3)

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("<slug:slug>/", views.post_details, name="post_details"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 
 def homepage(request):
     posts = Post.objects.all()
     context = {"posts": posts}
     return render(request, "blog/homepage.html", context)
+
+
+def post_details(request, slug):
+    post = Post.objects.filter(slug=slug)
+
+    if post.exists():
+        post = post[0]
+    else:
+        return HttpResponse("Page not found")
+
+    context = {"post": post}
+    return render(request, "blog/post_details.html", context)
