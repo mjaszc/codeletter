8865aad6dbdb3785ea03f977b7676d5e79690a33
jashchanin <111734497+jashchanin@users.noreply.github.com>
2022-11-11 22:00:58 +0100

    Added change account settings and logout (#7)

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Post
+from django.contrib.auth.models import AbstractUser
 
 
 class AddPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ("title", "content")
+
+
+class UserSettingsForm(forms.ModelForm):
+    class Meta:
+        model = AbstractUser
+        fields = ("username", "email", "first_name", "last_name")
