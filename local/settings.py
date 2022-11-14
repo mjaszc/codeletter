2a71d7b0e3d118c13251b6ff0eba29a95a3c0523
jashchanin <111734497+jashchanin@users.noreply.github.com>
2022-11-14 17:45:32 +0100

    Migrated to PostgreSQL (#11)

--- local/settings.py
+++ local/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = "local.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
