99cbf31ff2b6b01d50f5cc21f3f909854beb8b0c
mjasc <111734497+mjasc@users.noreply.github.com>
2022-11-15 19:38:06 +0100

    2nd fix attempt

--- local/settings.py
+++ local/settings.py
@@ -34,7 +34,7 @@ def get_env_variable(var_name):
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
