ea03a10d5a8c919f48fa92fd6f3a13af479d62e9
mjasc <111734497+mjasc@users.noreply.github.com>
2022-11-16 09:14:36 +0100

    fix1

--- local/settings.py
+++ local/settings.py
@@ -12,23 +12,22 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
+load_dotenv(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = os.environ.get("DEBUG")
+load_dotenv(DEBUG)
 
 ALLOWED_HOSTS = []
 
