80d6f5bc9c9c9ec2a4087c3fe2d5a321f32a0d37
mjasc <111734497+mjasc@users.noreply.github.com>
2022-11-16 10:19:50 +0100

    fix9

--- local/settings.py
+++ local/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-# from pathlib import Path
+from pathlib import Path
 import environ
 import os
 
@@ -20,9 +20,8 @@ env = environ.Env(
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
