30fe26fe11ada79513d8ca9aae0c75b2611f7712
mjasc <111734497+mjasc@users.noreply.github.com>
2022-12-08 19:42:53 +0100

    Added users url to the API

--- local/settings.py
+++ local/settings.py
@@ -168,3 +168,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+# REST Framework
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
