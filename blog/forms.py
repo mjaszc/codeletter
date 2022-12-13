e36d7afcc77405f45d4987d2563bfcd148d46b97
mjasc <111734497+mjasc@users.noreply.github.com>
2022-12-13 09:09:02 +0100

    Added custom register form

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,24 @@
 from django import forms
 from .models import Post, Comment, ProfileSettings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=25)
+    last_name = forms.CharField(max_length=25)
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+        )
 
 
 class AddPostForm(forms.ModelForm):
