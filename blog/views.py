096a1782017cd9767fa78f3e6d7569c014120609
mjasc <111734497+mjasc@users.noreply.github.com>
2023-01-02 22:19:50 +0100

    Deleted unused lines of code

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
-import json
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib import messages
-from .models import Post, Category, ProfileSettings, Notification
+from .models import Post, Category, ProfileSettings
 from django.contrib.auth.models import User
 from django.core.cache import cache
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .forms import (
     AddPostForm,
     AddCommentForm,
@@ -22,7 +21,6 @@ from django.contrib.auth import (
     update_session_auth_hash,
     get_user_model,
 )
-from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator
 from .forms import UserSettingsForm
 from django.db.models import Q
@@ -131,23 +129,6 @@ def post_details(request, slug):
         "liked": liked,
     }
     return render(request, "blog/post_details.html", context)
-
-
-# @csrf_exempt
-# def create_notification(request, slug):
-#     data = json.loads(request.body)
-#     post = Post.objects.filter(slug=slug)
-#
-#     user = request.user
-#     try:
-#         post = Post.objects.get(id=data.get("id"))
-#         new_notification = Notification(
-#             receiver_user=post.user, provider_user=user, title=post.title, post=post
-#         )
-#         new_notification.save()
-#         return JsonResponse({"message": "success", "status": True})
-#     except Post.DoesNotExist:
-#         return JsonResponse({"error": "Something went wrong", "status": False})
 
 
 def create_post(request):
