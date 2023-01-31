87ab1910d767b3616eb3b0dc7179b95fd473c0c8
mjasc <111734497+mjasc@users.noreply.github.com>
2023-01-31 15:36:48 +0100

    Added readtime

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.utils.text import slugify
 from django.utils.html import format_html
 from django.contrib.auth.models import User
+import readtime
 
 
 class Category(models.Model):
@@ -51,6 +52,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_readtime(self):
+        result = readtime.of_text(self.content)
+        return result.text
 
     def save(self, *args, **kwargs):
         """Automatically creates slug on save when you left slug empty"""
