567ed7188a7ae4da485a688effd210fb7d746e4f
mjasc <111734497+mjasc@users.noreply.github.com>
2023-03-21 08:09:41 +0100

    Added styling to the user settings page

--- blog/forms.py
+++ blog/forms.py
@@ -81,6 +81,30 @@ class AddPostForm(forms.ModelForm):
 
 
 class UserSettingsForm(forms.ModelForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "rounded-md bg-white border",
+                "placeholder": "Enter your username",
+            }
+        )
+    )
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "rounded-md", "placeholder": "Enter your first name"}
+        )
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "rounded-md", "placeholder": "Enter your last name"}
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={"class": "rounded-md", "placeholder": "user@example.com"}
+        )
+    )
+
     class Meta:
         model = User
         fields = ("username", "email", "first_name", "last_name")
