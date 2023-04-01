15bba8ab9bd947e56bcdd4243a0551b2ac21be41
mjasc <111734497+mjasc@users.noreply.github.com>
2023-04-01 23:15:23 +0200

    Removed lines in settings file

--- local/settings.py
+++ local/settings.py
@@ -23,14 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = config("SECRET_KEY", default="insecure")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 
