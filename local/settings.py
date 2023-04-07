7a7baa34bd3399ee75d3597f465b7f2d54ebb0ac
mjasc <111734497+mjasc@users.noreply.github.com>
2023-04-07 15:34:56 +0200

    Fixed deployment issue with static files

--- local/settings.py
+++ local/settings.py
@@ -178,9 +178,7 @@ CACHES = {
 
 STATIC_URL = "staticfiles/"
 
-if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+if not DEBUG:
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
