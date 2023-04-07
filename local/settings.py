6bd0c7dc4b4ea16c19ef8852683dd8bac763c73a
mjasc <111734497+mjasc@users.noreply.github.com>
2023-04-07 15:41:16 +0200

    Added accidentally deleted line with static_root

--- local/settings.py
+++ local/settings.py
@@ -176,6 +176,8 @@ CACHES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATIC_URL = "staticfiles/"
 
 if not DEBUG:
