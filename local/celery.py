65198cf31e1e456dde06f9a94cf84e46aa01fbf9
jasmar2 <111734497+jasmar2@users.noreply.github.com>
2023-04-12 20:45:35 +0200

    Set up files for deployment on flyio (#96)

--- local/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local.settings")
-
-app = Celery("blog")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
