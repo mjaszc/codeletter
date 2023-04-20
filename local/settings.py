30615b809ddeafaf3a8f120ef27a08b5f5d4e9e7
jasmar2 <111734497+jasmar2@users.noreply.github.com>
2023-04-20 18:09:19 +0200

    Modified settings file

--- local/settings.py
+++ local/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", default="insecure")
+SECRET_KEY = config("SECRET_KEY")
 
 
 ALLOWED_HOSTS = ["*"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 
 # Application definition
