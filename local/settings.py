2bf6fe2c7de6c6f1170ae8f1858992973a60b0d6
jasmar2 <111734497+jasmar2@users.noreply.github.com>
2023-04-23 11:55:29 +0200

    Fixed allowed hosts production error (#111)

--- local/settings.py
+++ local/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default="insecure")
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://codeletter.fly.dev']
+ALLOWED_HOSTS = ['codeletter.fly.dev', 'localhost', '127.0.0.1']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
